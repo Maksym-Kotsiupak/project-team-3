93bc0bbfd1f18dd3d1cff9663763cb0145d9ade9
Maksym-Kotsiupak <131818079+Maksym-Kotsiupak@users.noreply.github.com>
2023-07-05 13:41:27 +0300

    advadvadva

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,19 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
+      openSecModalBtn: document.querySelector("[data-modal-open-sec]"),
+      openThirdModalBtn: document.querySelector("[data-modal-open-third]"),
+      openFourModalBtn: document.querySelector("[data-modal-open-four]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
-  
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openSecModalBtn.addEventListener("click", toggleModal);
+  refs.openThirdModalBtn.addEventListener("click", toggleModal);
+  refs.openFourModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      console.log('first')
     }
   })();
